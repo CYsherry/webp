8d1923f0ed48a54b1c2d05a7be355a2deb0d8c57
CYsherry <100269330+CYsherry@users.noreply.github.com>
2022-04-26 21:00:51 +0800

    Add files via upload

---
+++ hw5/hw5.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    loadDoc();
+})
+function loadDoc(){
+    $.ajax({
+        url:"https://ptx.transportdata.tw/MOTC/v2/Rail/Metro/ODFare/TYMC?%24top=420&%24format=JSON",
+        type:"get",
+        dataType:"json",
+        success:function(info){
+            console.log(info);
+            for(var i=0; i<400; i+=21 ){
+                $("#trainTable").append(
+                    "<tr>"
+                    +"<td>"+info[i].OriginStationName.Zh_tw+"</td>"
+                    +"<td>"+info[i].DestinationStationName.Zh_tw+"</td>"
+                    +"<td>"+info[i].Fares[0].Price+"</td>"
+                    +"<td>"+info[i].TravelTime+"</td>"            
+                    +"</tr>");
+            }
+        },
+        error:function(data){
+            alert("error");
+        }
+    })
+}
